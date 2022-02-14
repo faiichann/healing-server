d92705e5bf1624259e5dc13d8c9b781586d6520e
faiichannn <fainichkamon@gmail.com>
2022-02-15 00:37:15 +0700

    create api

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const express = require("express");
-
-const PORT = process.env.PORT|| 5000;
 const app = express();
-const tests =[
-    {id: 1,name: "faii"},
-    {id: 2,name:"iiaf"}
-];
-app.get('/api/tests/:id', function(req,res){
-    const id = req.params.id;
-    if(id==1){
-        res.send(tests[0]);
-    }else if(id==2){
-        res.send(tests[1]);
-    }else{
-        res.send('error 404');
-    }
-})
-app.get('/api/tests', function(req,res){
-    res.send(tests);
+const result = require('./db')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+const PORT = process.env.PORT|| 5000;
+const cors = require("cors");
+app.use(cors());
+
+app.get('/results', (req, res) => {
+  res.json(result)
 })
 
+app.get('/results/:id', (req, res) => {
+    res.json(result.find(result => result.id === req.params.id))
+})
+
+app.post('/results', (req, res) => {
+    result.push(req.body)
+    res.status(201).json(req.body)
+})
+
+app.put('/results/:id', (req, res) => {
+    const updateIndex = result.findIndex(result => result.id === req.params.id)
+    res.json(Object.assign(result[updateIndex], req.body))
+  })
+
+  
+app.delete('/results/:id', (req, res) => {
+    const deletedIndex = books.findIndex(result => result.id === req.params.id)
+    books.splice(deletedIndex, 1)
+    res.status(204).send()
+ })
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
 });
